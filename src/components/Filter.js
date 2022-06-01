8507e35507a7f58068149dd8e4d560296e291f93
Phung Dinh Son <62043279+phungson185@users.noreply.github.com>
2022-06-01 23:38:51 +0700

    add-tabs (#4)

--- src/components/Filter.js
+++ src/components/Filter.js
@@ -1,12 +1,33 @@
 /* 
   【Filterコンポーネント】
-　・該当するTodoをステータス毎にで分けてリスト表示する
-　・タブで表示する
-　・サポートするステータスは「すべて」「未完了」「完了済み」
+ ・該当するTodoをステータス毎にで分けてリスト表示する
+ ・タブで表示する
+ ・サポートするステータスは「すべて」「未完了」「完了済み」
 */
-function Filter(  ) {
+function Filter({ value, onChange }) {
+
+  const handleClick = (key, e) => {
+    e.preventDefault();
+    onChange(key);
+  };
+
   return (
     <div className="panel-tabs">
+      <a
+        href="#"
+        onClick={e => handleClick('ALL', e)}
+        className={value === 'ALL' ? 'is-active' : ''}
+      >全て</a>
+      <a
+        href="#"
+        onClick={e => handleClick('TODO', e)}
+        className={value === 'TODO' ? 'is-active' : ''}
+      >未完了</a>
+      <a
+        href="#"
+        onClick={e => handleClick('DONE', e)}
+        className={value === 'DONE' ? 'is-active' : ''}
+      >完了済み</a>
     </div>
   );
 }
